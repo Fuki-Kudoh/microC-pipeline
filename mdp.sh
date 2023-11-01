562fce58b49111000208d8cfba512fb43adf6cd4
Fuki Kudoh <55123235+Fuki-Kudoh@users.noreply.github.com>
2023-11-01 15:27:51 -0400

    Update mdp.sh

--- mdp.sh
+++ mdp.sh
@@ -2,13 +2,17 @@
 
 echo ${date}
 sample_ID=$1
+genome_name=$2
+genome=$3
+option=$4
 
 ##############################
 #This is for alignment.
-#sbatch --time=24:00:00 --cpus-per-task=64 --mem=64g microC.sh <sample_ID>
-#directory-+-fastq--+--{sample_ID}_R1.fastq.gz
-#          |        +--{sample_ID}_R2.fastq.gz
+#sbatch --time=24:00:00 --cpus-per-task=64 --mem=64g microC.sh <sample_ID> <genome> <genome location> [option]
+#directory-+-fastq--+--{sample_ID}_R1_001.fastq.gz
+#          |        +--{sample_ID}_R2_001.fastq.gz
 #          +-BAM
+#          +-fastqc
 #          +-hic
 #          +-pairs
 #          +-stats
@@ -17,14 +21,17 @@ sample_ID=$1
 
 module load samtools bwa pairtools preseq juicer
 mkdir temp/${sample_ID}
+
+trim_galore -j 4 --fastqc_args "--outdir fastqc" --paired fastq/${sample_ID}_R1.001.fastq.gz fastq/${sample_ID}_R2.001.fastq.gz
+
 #alignment -t is thread number, 30min
-bwa mem -5SP -T0 -t64 /fdb/bwa/indexes/mm10.fa \
+bwa mem -5SP -T0 -t64 ${genome}.fa \
 fastq/${sample_ID}_R1_001.fastq.gz fastq/${sample_ID}_R2_001.fastq.gz \
 -o temp/${sample_ID}/${sample_ID}.sam
 
 #parse --nproc-in and --nproc-out is thread number, 1.5h
 pairtools parse --min-mapq 40 --walks-policy 5unique --max-inter-align-gap 30 \
---nproc-in 64 --nproc-out 64 --chroms-path /fdb/bwa/indexes/mm10.fa \
+--nproc-in 64 --nproc-out 64 --chroms-path ${genome}.fa \
 temp/${sample_ID}/${sample_ID}.sam > temp/${sample_ID}/${sample_ID}.pairsam
 #sort --nproc is thread number. 40 min
 rm temp/${sample_ID}/${sample_ID}.sam
@@ -52,14 +59,14 @@ samtools index BAM/${sample_ID}.PT.bam
 preseq lc_extrap -bam -pe -extrap 2.1e9 -step 1e8 -seg_len 1000000000 \
 -output stats/comp_${sample_ID}.txt BAM/${sample_ID}.PT.bam
 #.hic contact map
-juicer_tools pre -j 64 pairs/${sample_ID}.pairs hic/${sample_ID}.hic mm10
+juicer_tools pre -j 64 pairs/${sample_ID}.pairs hic/${sample_ID}.hic ${genome_name}
 
 #cooltools
 #module load cooltools
 #bgzip pairs/${sample_ID}.pairs
 #pairtools sort --nproc 64 -o pairs/${sample_ID}.sorted.pairs.gz pairs/${sample_ID}.pairs.gz
 #rm pairs/${sample_ID}.pairs
-#cooler cload pairix -p 64 /fdb/bwa/indexes/mm10.fa:1000 pairs/${sample_ID}.sorted.pairs.gz cooler/${sample_ID}.cool
+#cooler cload pairix -p 64 ${genome}:1000 pairs/${sample_ID}.sorted.pairs.gz cooler/${sample_ID}.cool
 
 #######################
 # Version history and updating schedule
